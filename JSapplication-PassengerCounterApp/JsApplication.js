5a4fc9c7259f6c79d510eb4064d55259746e88d1
Neha Gawande <157957753+Neha-Gawande111@users.noreply.github.com>
2024-02-06 22:40:06 +0530

    Add files via upload

---
+++ JSapplication-PassengerCounterApp/JsApplication.js
@@ -0,0 +1,27 @@
+// document.getElementById("count").innerText=5;
+// let count = 0;
+
+// console.log(myAge) //here we get reference error: before initialization u cant access variable
+// 1. create variable myage and value of age to it
+// let myAge = 25;
+// document.write(myAge);
+// 2.log the myage variable to the console
+// console.log(myAge)
+// *-------------------------------------------------------*/
+
+//  let counts=5;
+//   counts=count+1;
+//   console.log(counts)
+//   counts=counts+1;
+//   document.write(counts)
+
+// ********************************************************************************************************************************************
+// listen click for button
+// increment the count variable when we click to increment button
+
+function increment()
+{
+    console.log("button was clicked");
+}
+
+
